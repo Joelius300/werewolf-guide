24572834745127dc14c8cbf497805b2beab0d25c
Joel Liechti <bstlng321@gmail.com>
2021-09-06 18:07:33 +0200

    Fix manifest path

--- guide/.vuepress/config.ts
+++ guide/.vuepress/config.ts
@@ -15,7 +15,7 @@ export default defineUserConfig<DefaultThemeOptions>({
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'images/favicon-32x32.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'images/favicon-16x16.png' }],
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: 'images/apple-touch-icon.png' }],
-    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['link', { rel: 'manifest', href: 'manifest.webmanifest' }],
     ['meta', { name: 'theme-color', content: '#fff' }],
   ],
   themeConfig: {
