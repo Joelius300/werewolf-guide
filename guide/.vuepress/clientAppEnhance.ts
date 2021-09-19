088a484295512b76cc3069a39cc2cd8ef35681f1
Joel Liechti <bstlng321@gmail.com>
2021-09-19 18:32:11 +0200

    The redirects already work on GH Pages, nice

--- guide/.vuepress/clientAppEnhance.ts
+++ guide/.vuepress/clientAppEnhance.ts
@@ -2,8 +2,11 @@ import { defineClientAppEnhance } from '@vuepress/client';
 
 export default defineClientAppEnhance(({ router }) => {
   // define redirects. These aren't prerendered and if you're on one and refresh the page, you'll get a 404.
-  // to prevent this you have to define the redirects on your webserver as well.
-  // in GitHub Pages, you'd do this with https://spa-github-pages.rafgraph.dev/ probably.
+  // ~~to prevent this you have to define the redirects on your webserver as well.~~
+  // ~~in GitHub Pages, you'd do this with https://spa-github-pages.rafgraph.dev/ probably.~~
+  // Amazingly, it already works out of the Box (probably VuePress being awesome again).
+  // The only not accounted for is SEO, as described here: https://github.com/rafgraph/spa-github-pages#seo
+  
   router.addRoute({
     path: '/rules/characters/',
     redirect: '/rules/characters/villager.html',
